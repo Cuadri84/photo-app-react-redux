06a5c94f901760e118500d2753b02283b67d95b3
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-05-03 15:28:48 +0200

    Updates

--- src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import searchReducer from "../features/search/SearchSlice";
-import favoriteReducer from "../features/favoritePhotos/favoritePhotosSlice";
-
-export const store = configureStore({
-  reducer: { search: searchReducer, favorite: favoriteReducer },
-});
